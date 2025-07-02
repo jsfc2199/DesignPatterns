331eca71320ca93616c6079ecf9216ead797c31e
Juan Sebastian Franco Cervera <jsfc2199@gmail.com>
2025-07-02 17:59:31 -0500

    Ejemplo de uso de factory method

--- patterns/02-factoryMethod.ts
+++ patterns/02-factoryMethod.ts
@@ -30,6 +30,12 @@ class BeefHamburger implements IHamburger {
   }
 }
 
+class BeanHamburger implements IHamburger {
+  prepare(): void {
+    console.log("Preparando hamburguesa de frijoles");
+  }
+}
+
 //Ahora queremos crear un  restaurante que entregue ambas hamburguesas
 //Es abstracta porque no me interesa hacer un new Restaurant, pero sirve para definir el esqueleto de otras clases
 abstract class Restaurant {
@@ -53,3 +59,39 @@ class BeefRestaurant extends Restaurant {
     return new BeefHamburger();
   }
 }
+
+class BeanRestaurant extends Restaurant {
+  override createHamburger(): IHamburger {
+    return new BeanHamburger();
+  }
+}
+
+//utilización
+function main() {
+  let restaurant: Restaurant;
+
+  const burgerType = prompt(
+    "¿Qué tipo de hamburguesa quieres? (chicken o beef o bean)"
+  );
+
+  //no especificamos como se construye la hamburguesa, solo llamamos al restaurante
+  //no hacemos ningún new Hamburger o algo parecido
+  switch (burgerType) {
+    case "chicken":
+      restaurant = new ChickenRestaurant();
+      break;
+    case "beef":
+      restaurant = new BeefRestaurant();
+      break;
+    case "bean":
+      restaurant = new BeanRestaurant();
+      break;
+    default:
+      console.log("Tipo de hamburguesa no válido");
+      return;
+  }
+
+  restaurant.orderHamburger()
+}
+
+main();
